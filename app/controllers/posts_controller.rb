d0ee1671f53dc6375d46a30f758e732eec427f6a
lianaschapiro <liana.schapiro@gmail.com>
2016-01-12 13:14:51 -0500

    Built out posts controller and views. Fixes #2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,54 @@
 class PostsController < ApplicationController
+  
   def index
-  end
+		@posts = Post.all
+		@user = current_user
+	end
+
+	def show
+		@post = Post.find(params[:id])
+		@user = current_user
+	end
+
+	def new
+		@post = Post.new
+		@user = current_user
+	end
+
+	def create
+		@user = current_user
+		@post = Post.new(params[:post]).merge(user_id: @user.id)
+		if @post.save
+			flash[:notice] = "Thanks for posting!"
+			redirect_to posts_path
+		else
+			flash[:notice] = "Unable to post...try again?"
+			redirect_to new_post_path
+		end
+	end
+
+	def edit
+		@user = current_user
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@user = current_user
+		@post = Post.find(params[:id])
+		@post.update(params[:post])
+		@post.save
+		flash[:notice] = "Thanks for editing"
+		redirect_to posts_path
+	end
+
+	def destroy
+		@user = current_user
+		@post = Post.find(params[:id])
+		if @post.destroy
+			flash[:notice] = "Post destroyed"
+		else
+			flash[:notice] = "Unable to destoy post"		
+		end
+		redirect_to posts_path
+	end
 end

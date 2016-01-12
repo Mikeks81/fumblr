6e666e9e38df7b9420ca1d94b2cf95f4f8051e4f
Michael <Mikeks81@gmail.com>
2016-01-12 14:02:31 -0500

    added links to home#home, input field to create new user and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
   def index
+  	@user = current_user
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+	puts params.inspect
+	@user = User.new(params[:user])
+  	if @user.save
+    redirect_to posts_path 
+  	else
+    redirect_to new_user_path
+  	end
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'home#home'
 
-  
+  get '/log_in' => 'sessions#new'
+  post '/log_in' => 'sessions#create'  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

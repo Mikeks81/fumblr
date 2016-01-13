3c24a7b374ed008301f93329f42363127ca86e2a
Michael <Mikeks81@gmail.com>
2016-01-13 10:54:28 -0500

    flash message, user_params,session.clear on destroy, things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,10 @@ class UsersController < ApplicationController
   end
 
   def create
-	@user = User.new(params[:user])
+	@user = User.new(user_params)
   	if @user.save
   	session[:user_id] = @user.id
+    flash[:notice] = "Thanks for joining!"
     redirect_to posts_path 
   	else
     redirect_to new_user_path
@@ -37,11 +38,17 @@ class UsersController < ApplicationController
   def destroy
     @user = current_user
     if @user.destroy
+      session.clear
       flash[:notice] = "This user was deleted"
     else
       flash[:notice] = "Unable to delete the user"
     end
     redirect_to root_path
   end
+  
+  private   
+   def user_params
+      params.require(:user).permit(:fname, :lname, :username, :email, :password)   
+   end
 
 end

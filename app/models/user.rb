d661fb95e3ebfd23880f828db2dd333a8bc4364f
Michael <Mikeks81@gmail.com>
2016-01-15 11:45:36 -0500

    incomplete error message implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :posts, dependent: :destroy
 	has_many :comments, dependent: :destroy
+	validates :email, presence: true, uniqueness: true 
+	validates :username, presence: true, length: {in: 4..30}, uniqueness: true 
+	validates :password, presence: true, length: {in: 6..8}
 end
